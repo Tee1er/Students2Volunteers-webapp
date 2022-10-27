bc3ba433b5a98a7540d47e6c32b9234e0631fa77
Tyler S <tyler.x.song@gmail.com>
2022-10-27 17:38:18 -0400

    basic setup

---
+++ src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<script>
+// TBH just ignore this for now
+</script>
+
+<template>
+    <div class="root">
+        <!--HTML code goes here-->
+    </div>
+</template>
+
+<style scoped>
+/*CSS code goes here*/
+</style>
